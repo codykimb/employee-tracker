fa7beb4c87311955ec0c1dc9cf7c9ef3c4ee3618
Cody Kimb <codykimbell@gmail.com>
2021-03-10 22:07:22 -0600

    foreign keys

--- schema.sql
+++ schema.sql
@@ -3,23 +3,26 @@ DROP TABLE IF EXISTS roles;
 DROP TABLE IF EXISTS employee;
 
 
-CREATE TABLE departments (
-  id INT PRIMARY KEY,
+CREATE TABLE department (
+  id INT AUTO_INCREMENT PRIMARY KEY ,
   name VARCHAR(30) NOT NULL,
 );
 
-CREATE TABLE roles (
-  id INT PRIMARY KEY,
+CREATE TABLE role (
+  id INT AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
   department_id INT NOT NULL,
+  FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
-CREATE TABLE employees (
-  id INT PRIMARY KEY NOT NULL,
+CREATE TABLE employee (
+  id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL,
   manager_id INT
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
 
